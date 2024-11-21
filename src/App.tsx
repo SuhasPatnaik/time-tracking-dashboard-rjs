3b06650b98e351f254116bf97092287ac3027876
Suhas Patnaik <suhaspatnaik111@gmail.com>
2024-11-21 15:12:12 +0530

    Name card component ready + reset.css included

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,24 @@
+import "./App.css";
+
+import profilepic from "./assets/images/image-jeremy.png";
+
 export default function App() {
-  console.log("App.tsx file...");
-  return <div>Hello world</div>;
+  return (
+    <div className="name-card">
+      <div className="user-card">
+        <img
+          src={profilepic}
+          alt="Headshot of Jeremy Robson"
+          className="profile-pic"
+        />
+        <p>Report for</p>
+        <h1>Jeremy Robson</h1>
+      </div>
+      <div className="interval-options">
+        <p>Daily</p>
+        <p>Weekly</p>
+        <p>Monthly</p>
+      </div>
+    </div>
+  );
 }
