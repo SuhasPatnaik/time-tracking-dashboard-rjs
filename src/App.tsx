7a5f2cea6fcc623eb9e2c2aab542c91b123bf97d
Suhas Patnaik <suhaspatnaik111@gmail.com>
2024-11-22 00:27:43 +0530

    Created a map for activity specific styles, bgcolor & img src, state var for toggling b/w timeframe options

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
 import "./App.css";
 
 import profilepic from "./assets/images/image-jeremy.png";
-import workIcon from "./assets/images/icon-work.svg";
 import elipsisIcon from "./assets/images/icon-ellipsis.svg";
+import { useState } from "react";
+
+const activityStyles = {
+  Work: {
+    bgColor: "var(--clr-accent-work)",
+    imgSrc: "./assets/images/icon-work.svg",
+  },
+  Play: {
+    bgColor: "var(--clr-accent-play)",
+    imgSrc: "./assets/images/icon-play.svg",
+  },
+  Study: {
+    bgColor: "var(--clr-accent-study)",
+    imgSrc: "./assets/images/icon-study.svg",
+  },
+  Exercise: {
+    bgColor: "var(--clr-accent-exercise)",
+    imgSrc: "./assets/images/icon-exercise.svg",
+  },
+  Social: {
+    bgColor: "var(--clr-accent-social)",
+    imgSrc: "./assets/images/icon-social.svg",
+  },
+  SelfCare: {
+    bgColor: "var(--clr-accent-selfcare)",
+    imgSrc: "./assets/images/icon-self-care.svg",
+  },
+};
 
 export default function App() {
+  const [timeframe, setTimeframe] = useState("daily");
+
+  const handleTimeframeChange = (selectedTimeframe) => {
+    setTimeframe(selectedTimeframe);
+  };
+
   return (
     <>
       <div className="main-section">
@@ -19,9 +52,27 @@ export default function App() {
             <h1>Jeremy Robson</h1>
           </div>
           <div className="timeframe-options">
-            <button>Daily</button>
-            <button>Weekly</button>
-            <button>Monthly</button>
+            <button
+              onClick={() => {
+                handleTimeframeChange("daily");
+              }}
+            >
+              Daily
+            </button>
+            <button
+              onClick={() => {
+                handleTimeframeChange("weekly");
+              }}
+            >
+              Weekly
+            </button>
+            <button
+              onClick={() => {
+                handleTimeframeChange("monthly");
+              }}
+            >
+              Monthly
+            </button>
           </div>
         </div>
         <ActivityTimeCard />
